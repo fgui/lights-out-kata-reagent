9dfbae57121c218b237ac0909e41460335e7b50a
francesc <francesc.guillen@gmail.com>
2015-05-23 00:54:00 +0200

    show green on lights out. mirror 3x3 grid.

--- src/cljs/lights_out/core.cljs
+++ src/cljs/lights_out/core.cljs
@@ -27,50 +27,48 @@
 
 ;; assumes square box
 (defn neighbors [pos-x-y max]
-  (filter (partial out-grid 0 max) (map add-pos (repeat pos-x-y) neighbor))
-)
+  (filter (partial out-grid 0 max) (map add-pos (repeat pos-x-y) neighbor)))
 
 (defn flip [lights [x y]]
   (let [n (neighbors [x y] (dec (count lights)))]
-    (reduce flip-light lights n)
-    )
-)
+    (reduce flip-light lights n)))
 
+(defn win? [lights]
+  (every? zero?  (flatten lights)))
 ;; -------------------------
 ;; Views
-
 
 (defn light-component [a val x y]
   ^{:key x}
   [:div {:style {:float :left
                  :cursor :pointer
-                 :margin "1px"
+                 :margin "2px"
                  :padding "15px"
                  :font-weight :bold
                  :background-color (if (= 1 val) "#AAA" "#EEE")}
          :on-click #(swap! a flip [x y])
-         } val]
-)
+         } val])
 
 (defn row-component [a r-lights y]
   ^{:key y}
   [:div {:style {:clear :both}}
    (map (partial  light-component a)
-        r-lights (iterate inc 0) (repeat y))]
-)
+        r-lights (iterate inc 0) (repeat y))])
 
 (defn lights-component [a-lights]
-  [:div {:style {:float :left :margin "10px"} }
-   (map (partial row-component a-lights) @a-lights (iterate inc 0))
-   ]
-)
+  [:div {:style {:float :left
+                 :margin "10px"
+                 :padding "3px"
+                 :background-color (if (win? @a-lights) :green :white)}}
+   (map (partial row-component a-lights) @a-lights (iterate inc 0))])
 
 (defn current-page []
   [:div [:h3 "lights out!!"]
-   [lights-component (atom [[1 1 1][1 1 1][1 1 1]])]
+   (let [a (atom [[1 1 1][1 1 1][1 1 1]])]
+     [:div [lights-component a] [lights-component a]] ;;mirror components
+     )
    [lights-component (atom (vec (repeat 4 [1 1 1 1])))]
-   [lights-component (atom (vec (repeat 5 [1 1 1 1 1])))]
-   ])
+   [lights-component (atom (vec (repeat 5 [1 1 1 1 1])))]])
 
 ;; -------------------------
 ;; Initialize app
